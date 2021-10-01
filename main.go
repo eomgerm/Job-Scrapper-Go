406724c47edd114051b26c8e33c9ed9aeead3137
khh7052 <59150989+khh7052@users.noreply.github.com>
2021-10-01 16:18:18 +0900

    Routing web server feature

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"./scrapper"
+	"Job-Scrapper/scrapper"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
 )
 
+var fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
+
 func main() {
-	scrapper.Scrape("python")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
